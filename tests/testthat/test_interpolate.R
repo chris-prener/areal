abe9f8423389903f17a49cb36044026639238efc
Christopher Prener <chris.prener@slu.edu>
2018-12-14 09:47:09 -0600

    update so that unit tests now pass

--- tests/testthat/test_interpolate.R
+++ tests/testthat/test_interpolate.R
@@ -3,28 +3,28 @@ context("test aw_interpolate function")
 # load test data ------------------------------------------------
 
 # source data
-race <- aw_stl_race
-asthma <- aw_stl_asthma
+data(aw_stl_race, package = "areal")
+data(aw_stl_asthma, package = "areal")
 
 # target data
-wards <- aw_stl_wards
+data(aw_stl_wards, package = "areal")
 
 # create comparison data
-totalCompare <- sf::st_interpolate_aw(race["TOTAL_E"], wards, extensive = TRUE)
-asthmaCompare <- sf::st_interpolate_aw(asthma["ASTHMA"], wards, extensive = FALSE)
+totalCompare <- sf::st_interpolate_aw(aw_stl_race["TOTAL_E"], aw_stl_wards, extensive = TRUE)
+asthmaCompare <- sf::st_interpolate_aw(aw_stl_asthma["ASTHMA"], aw_stl_wards, extensive = FALSE)
 
 # test errors ------------------------------------------------
 
 # test results ------------------------------------------------
 
-totalResult1 <- aw_interpolate(wards, tid = WARD, source = race, sid = "GEOID", type = "extensive",
-                               weight = "sum", output = "sf", "TOTAL_E")
+totalResult1 <- aw_interpolate(aw_stl_wards, tid = WARD, source = aw_stl_race, sid = "GEOID",
+                               type = "extensive", weight = "sum", output = "sf", "TOTAL_E")
 
-totalResult2 <- aw_interpolate(wards, tid = WARD, source = race, sid = "GEOID", type = "extensive",
-                               weight = "total", output = "sf", "TOTAL_E")
+totalResult2 <- aw_interpolate(aw_stl_wards, tid = WARD, source = aw_stl_race, sid = "GEOID",
+                               type = "extensive", weight = "total", output = "sf", "TOTAL_E")
 
-asthmaResult <- aw_interpolate(wards, tid = WARD, source = asthma, sid = "GEOID", type = "intensive",
-                               output = "sf", weight = "sum", "ASTHMA")
+asthmaResult <- aw_interpolate(aw_stl_wards, tid = WARD, source = aw_stl_asthma, sid = "GEOID",
+                               type = "intensive", output = "sf", weight = "sum", "ASTHMA")
 
 test_that("interpolated values are equal", {
   expect_equal(totalCompare$TOTAL_E, totalResult2$TOTAL_E)
