12b30508faf09824928e87c91501034a8ba9e784
Christopher Prener <chris.prener@slu.edu>
2018-12-16 11:19:33 -0600

    finish testing for aw_weight

---
+++ tests/testthat/test_aw_weight.R
@@ -0,0 +1,49 @@
+context("test aw_weight function")
+
+# load test data ------------------------------------------------
+
+# target data
+data(aw_stl_wards, package = "areal")
+
+# source data
+data(aw_stl_race, package = "areal")
+
+# calculate intersection
+aw_stl_wards %>%
+  aw_intersect(source = aw_stl_race, areaVar = "...area") %>%
+  aw_total(source = aw_stl_race, id = GEOID, areaVar = "...area",
+           totalVar = "...totalArea", weight = "sum", type = "extensive")-> intersect
+
+# test errors ------------------------------------------------
+
+test_that("errors with missing objects", {
+  expect_error(aw_weight(ham, areaVar = "...area", totalVar = "...totalArea", areaWeight = "...areaWeight"),
+               "object 'ham' not found")
+})
+
+test_that("errors with missing parameters", {
+  expect_error(aw_weight(areaVar = "...area", totalVar = "...totalArea", areaWeight = "...areaWeight"),
+               "A sf object containing intersected data must be specified for the '.data' argument.")
+  expect_error(aw_weight(intersect, totalVar = "...totalArea", areaWeight = "...areaWeight"),
+               "A variable name must be specified for the 'areaVar' argument.")
+  expect_error(aw_weight(intersect, areaVar = "...area", areaWeight = "...areaWeight"),
+               "A variable name must be specified for the 'totalVar' argument.")
+  expect_error(aw_weight(intersect, areaVar = "...area", totalVar = "...totalArea"),
+               "A variable name must be specified for the 'areaWeight' argument.")
+})
+
+test_that("errors with misspecified parameters", {
+  expect_error(aw_weight(intersect, areaVar = "ham", totalVar = "...totalArea", areaWeight = "...areaWeight"),
+               "Variable 'ham', given for the area, cannot be found in the given intersected object.")
+  expect_error(aw_weight(intersect, areaVar = "...area", totalVar = "ham", areaWeight = "...areaWeight"),
+               "Variable 'ham', given for the total area, cannot be found in the given intersected object.")
+})
+
+# test inputs ------------------------------------------------
+
+test_that("correctly specified functions execute without error", {
+  expect_error(aw_weight(intersect, areaVar = "...area", totalVar = "...totalArea",
+                         areaWeight = "...areaWeight"), NA)
+  expect_error(aw_weight(intersect, areaVar = ...area, totalVar = ...totalArea,
+                         areaWeight = ...areaWeight), NA)
+})
