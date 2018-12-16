574edc28ac8ac2a21dd5b088c2425ba7e31c900c
Christopher Prener <chris.prener@slu.edu>
2018-12-16 12:09:15 -0600

    add lwgeom to list of imports

--- tests/testthat/test_aw_validate.R
+++ tests/testthat/test_aw_validate.R
@@ -52,6 +52,7 @@ aw_stl_wards_conflict <- dplyr::mutate(aw_stl_wards, WHITE_E = FALSE)
 test_that("validation result is false", {
   expect_equal(aw_validate(source = racedf, target = wardsdf, varList = "TOTAL_E"), FALSE)
   expect_equal(aw_validate(source = aw_stl_race, target = wardsdf, varList = "TOTAL_E"), FALSE)
+  expect_equal(aw_validate(source = racedf, target = aw_stl_wards, varList = "TOTAL_E"), FALSE)
   expect_equal(aw_validate(source = aw_stl_race, target = wards83, varList = "TOTAL_E"), FALSE)
   expect_equal(aw_validate(source = aw_stl_race, target = aw_stl_wards, varList = "WARD"), FALSE)
   expect_equal(aw_validate(source = aw_stl_race, target = aw_stl_wards_conflict, varList = "WHITE_E"), FALSE)
@@ -61,3 +62,9 @@ test_that("verbose option creates tibble", {
   expect_equal(class(aw_validate(source = aw_stl_race, target = aw_stl_wards,
                                         varList = "TOTAL_E", verbose = TRUE))[1], "tbl_df")
 })
+
+invalidV1 <- aw_validate(source = racedf, target = wardsdf, varList = "TOTAL_E", verbose = TRUE)
+invalidV2 <- aw_validate(source = aw_stl_race, target = wardsdf, varList = "TOTAL_E", verbose = TRUE)
+invalidV3 <- aw_validate(source = racedf, target = aw_stl_wards, varList = "TOTAL_E", verbose = TRUE)
+
+
